8912242f64327dc930fc643cd4c675f0a00037df
Anant Jaiswal <jaiswalanant339@gmail.com>
2023-05-04 11:14:08 +0530

    title change

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -19,7 +19,7 @@ export default function Header() {
           </button>
 
           <a href="#" className="navbar-brand">
-            Known
+            AJ Multimedia Institute
           </a>
         </div>
 
